4b7c63d0e30a658db6977d981b7f7c7c9a5345b9
Qwerasten <qwerasten@gmail.com>
2022-04-03 15:55:39 +0300

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "linebreak-style": "off",
+    "no-plusplus": "off",
+    "no-useless-escape": "off",
+    "max-len": ["error", { ignoreComments: true }],
+    "import/extensions": [0, { js: "always" }],
+  },
+};
